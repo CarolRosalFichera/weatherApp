4c7a043dde7eb8d4786ecfb78d6f1a5a59c48fd4
AnaRosalRamalho <104014639+CarolRosalFichera@users.noreply.github.com>
2022-04-21 12:54:17 -0500

    Axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 let apiKey = "13eb9aa094479d1e47337cf35fcd3f28";
 
-let city = "london";
+let city = "sao paulo";
 
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+function ShowTemperature(response) {
+    console.log(response)
+}
+
+
+axios.get(apiUrl).then(ShowTemperature);
