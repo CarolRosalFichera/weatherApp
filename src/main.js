4bf1ea61bbed28d16e7a5ab852783df82fad9d8c
AnaRosalRamalho <104014639+CarolRosalFichera@users.noreply.github.com>
2022-04-22 11:51:49 -0500

    JS functions

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-// VARIABLES
-let apiKey = "13eb9aa094479d1e47337cf35fcd3f28";
-let city = "Austin";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+// API
+function showCity() {
+    let apiKey = "13eb9aa094479d1e47337cf35fcd3f28";
+    let city = document.querySelector(".search").value;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeatherDetails);
+}
+
+let btnSearch = document.querySelector(".submit");
+btnSearch.addEventListener('click', showCity);
 
 
 
+// CURRENT TIME INFO
 let now = new Date();
 let h1 = document.querySelector(".date");
 let date = now.getDate()
@@ -16,11 +23,7 @@ let day = days[now.getDay()];
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let month = months[now.getMonth()];
 
-
 h1.innerHTML = `${day} ${month} ${date}, ${hour}:${minutes}, ${year}`;
-
-
-
 
 
 
@@ -45,14 +48,29 @@ function showWeatherDetails(response) {
     //
 }
 
+function searchCity(event) {
+    event.preventDefault();
+    let cityElement = document.querySelector(".city-name")
+    let cityInput = document.querySelector(".search");
+    cityElement.innerHTML = cityInput.value;
+}
+
+function showLocation() {
+     let apiKey = "13eb9aa094479d1e47337cf35fcd3f28";
+}
+
+function getCurrentPosition() {
+    navigator.geolocation.getCurrentPosition(showLocation);
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeatherDetails);
+}
+
+
+let form = document.querySelector("#locationInput")
+form.addEventListener("submit", searchCity)
+
+
 
 
 // CALLS
     
-axios.get(apiUrl).then(showWeatherDetails);
-
-
-
-let cityInput = document.querySelector(".city-name")
-cityInput.innerHTML = city;
-
