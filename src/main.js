3a4bc6390410ca72f0282cbec2d1382dc4b11fa9
AnaRosalRamalho <104014639+CarolRosalFichera@users.noreply.github.com>
2022-04-27 13:54:12 -0500

    Daily Api created

--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ function showCity() {
     let apiKey = "13eb9aa094479d1e47337cf35fcd3f28";
     let city = document.querySelector(".search").value;
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
+
+
+
+
     axios.get(apiUrl).then(showWeatherDetails);
 }
 
-
+function getForecast(coordinates) {
+    console.log(coordinates)
+    let apiKey = "13eb9aa094479d1e47337cf35fcd3f28";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function showWeatherDetails(response) {
     console.log(response.data);
@@ -28,8 +38,10 @@ function showWeatherDetails(response) {
     iconElement.setAttribute('src', `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     //
     celsiusTemperature = response.data.main.temp;
-
-
+    //
+    
+    getForecast(response.data.coord)
+    
 }
 
 let btnSearch = document.querySelector(".submit");
@@ -59,7 +71,8 @@ h1.innerHTML = `${day} ${month} ${date}, ${hour}:${minutes}, ${year}`
 
 
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data.daily)
     let forecastElement = document.querySelector('#forecast') 
 
     let forecastHTML = `<div class="row">`;
@@ -144,6 +157,4 @@ function displayForecast() {
 
 
     
-
-displayForecast();
 
